266b7384417e862cde5014c945347213ee82550d
Samuel B <sambeau101@gmail.com>
2021-10-06 18:50:43 -0400

    Final Changes Made
    Could do some UI changes to make it easier for User to understand, but information is all there to digest

--- Assignment 1/Array.h
+++ Assignment 1/Array.h
@@ -5,18 +5,19 @@
 template<class T>
 class Array
 {
+private:
+
 public:
 	// Public Variables
 	T* m_array;			// Pointer to the beginning of the array
 
 	int m_maxSize;		// Maximum size of the array
-	int m_growSize;		// Amount the array can grow through expansion
-	int m_numElements;	// Number of items currently in my array
+	int m_growSize = 2;		// Amount the array can grow through expansion
+	int m_numElements = 0;	// Number of items currently in the array
 	
 
 	// Constructor
-	Array(int size) 
-		m_array(NULL), m_maxSize(0), m_growSize(2), m_numElements(0)
+	Array(int size) : m_array(NULL), m_maxSize(0), m_growSize(2), m_numElements(0)
 	{
 		if (size)	// Is this a legal size for an array?
 		{
@@ -57,8 +58,8 @@ public:
 	}
 
 	// Virtuals
-	virtual void push(T val) {};
-	virtual int search(T val) {};
+	virtual void push(T val) = 0;
+	virtual int search(T val) = 0;
 
 	// Overloaded [] operator
 	T& operator[](int index)
@@ -129,7 +130,178 @@ public:
 		temp = nullptr;
 
 		m_maxSize += m_growSize;
+		m_growSize *= 2;
 
 		return true;
 	}
+};
+
+// Ordered Array Template Class
+
+template<class T>
+class OrderedArray final : public Array<T>
+{
+private:
+
+public:
+	// Public Variables
+	bool m_dupesAllowed;	// Whether or not copies of data are allowed in the array
+
+	// Constructor 
+
+	OrderedArray(int size) : Array<T>(size)
+	{
+		std::cout << "Ordered Constructor." << std::endl;
+		m_dupesAllowed = false;
+	};
+
+	// Gets and Sets
+	int GetDupesAllowed()
+	{
+		return m_dupesAllowed;
+	}
+	int SetDupesAllowed(bool state)
+	{
+		m_dupesAllowed = state;
+	}
+
+	// Insertion -- Big-O = O(N)
+	void push(T val)
+	{
+		assert(this->m_array != nullptr);
+
+		if (((binarySearch(val, 0, this->m_numElements - 1)) != -1) && !GetDupesAllowed())	// Duplicate Check
+		{
+			std::cout << std::endl << std::endl << "Duplicate data found! Unable to enter value: " << val << std::endl << std::endl;
+			return;
+		}
+
+		if (this->m_numElements >= this->m_maxSize)
+		{
+			this->Expand();
+		}
+
+		int i, k;	// i - Index to be inserted. k - Used for shifting purposes
+		// Step 1: Find the index to insert val
+		for (i = 0; i < this->m_numElements; i++)
+		{
+			if (this->m_array[i] > val)
+			{
+				break;
+			}
+		}
+
+		// Step 2: Shift everything to the right of the index(i) forward by one. Work backwards
+		for (k = this->m_numElements; k > i; k--)
+		{
+			this->m_array[k] = this->m_array[k - 1];
+		}
+
+		// Step 3: Insert val into the array at index
+		this->m_array[i] = val;
+
+		this->m_numElements++;
+
+		// return i;
+	}
+
+	// Searching
+	// Binary Search
+	int search(T val)
+	{
+		// Call to binary search recursive function
+		// Binary Search: searchKey, initial lowerBound, initial upperBound
+		return binarySearch(val, 0, this->m_numElements - 1);
+	}
+
+private:
+	// Private functions
+	// Recursive Binary Search
+	int binarySearch(T val, int lowerBound, int upperBound)
+	{
+		assert(Array<T>::m_array != nullptr);
+		//assert(lowerBound >= 0);
+		///assert(upperBound < this->m_numElements);
+
+		// Bitwise divide by 2
+		int current = (lowerBound + upperBound) >> 1;
+
+		// Check 1 "Base case": Did we find the searchKey at the current index?
+		if (Array<T>::m_array[current] == val)
+		{
+			// We found it! Return the index
+			return current;
+		}
+		// Check 2 "Base base": Are we done searching? 
+		else if (lowerBound > upperBound)
+		{
+			// Did not find searchKey within m_array
+			return -1;
+		}
+		// Check 3: Which half of the array is searchKey in?
+		else
+		{
+			if (Array<T>::m_array[current] < val)
+			{
+				// Search the upper half of the array
+				return binarySearch(val, current + 1, upperBound);
+			}
+			else
+			{
+				// Search the lower half of the array
+				return binarySearch(val, lowerBound, current - 1);
+			}
+		}
+
+		return -1;
+	}
+
+};
+
+// Un-Ordered Templated Array Class
+
+template<class T>
+class UnorderedArray final : public Array<T>
+{
+private:
+
+public:
+	// Constructor 
+
+	UnorderedArray(int size) : Array<T>(size) { std::cout << "Unordered Constructor." << std::endl; };
+
+	// Insertion
+	// Fast insertion for UnorderedArray -- Big-O is O(1)
+	void push(T val)
+	{
+		assert(Array<T>::m_array != nullptr); // Debugging purposes
+
+		if (Array<T>::m_numElements >= this->m_maxSize)	// Check if the array has to expand to accommodate the new data.
+		{
+			Array<T>::Expand();
+		}
+
+		// My array has space for a new value. Let's add it!
+		Array<T>::m_array[Array<T>::m_numElements] = val;
+		Array<T>::m_numElements++;
+	}
+
+	// Searching
+	// Linear Search
+	int search(T val)
+	{
+		assert(Array<T>::m_array != nullptr);
+
+		// Brute-force Search
+		for (int i = 0; i < Array<T>::m_numElements; i++)
+		{
+			if (Array<T>::m_array[i] == val)
+			{
+				return i;	// Return the index of where the item is located in the array
+			}
+		}
+
+		return -1;
+	}
+
 };
--- Assignment 1/Main.cpp
+++ Assignment 1/Main.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <assert.h>
 
-#include "OrderedArray.h"
-#include "UnorderedArray.h"
+#include "Array.h"
+
+using namespace std;
 
 int main()
 {
-	OrderedArray<int> OrderArray(3);
-
-	Array<double> array(2);
+	OrderedArray<int> OrderArray(2);
 	
 	UnorderedArray<float> UnorderArray(4);
 
-	OrderArray.push(2);
+	OrderArray.push(10);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
 	OrderArray.push(4);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
 	OrderArray.push(6);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	OrderArray.push(4);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	OrderArray.push(5);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	OrderArray.push(12);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	OrderArray.push(7);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	OrderArray.push(14);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	OrderArray.push(24);
+	cout << "OrderArray size: " << OrderArray.GetMaxSize() << endl;
+	
+	cout << "------------------------------------------------------------------\n";
+	cout << endl << "Ordered Array Contents:" << endl << endl;
+	cout << "------------------------------------------------------------------\n";
 
-	for (int i = 0; i < OrderArray; i++)
+	for (int i = 0; i < OrderArray.GetSize(); i++)
 	{
-		// Gotta figure out the basics again lol
+		cout << "Element at Index " << i << " is: " << OrderArray[i] << endl;
 	}
 
 	UnorderArray.push(1.3f);
 	UnorderArray.push(3.3f);
 	UnorderArray.push(6.8f);
 	UnorderArray.push(9.2f);
+
+	cout << "------------------------------------------------------------------\n";
+	cout << endl << "Un-Ordered Array Contents:" << endl << endl;
+	cout << "------------------------------------------------------------------\n";
+
+	for (int i = 0; i < UnorderArray.GetSize(); i++)
+	{
+		cout << "Element at Index " << i << " is: " << UnorderArray[i] << endl;
+	}
+
+
 	return 0;
 }
